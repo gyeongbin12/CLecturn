2df38a03ca7ea17930f1d65a9a7c3698eababf01
gyeongbin06 <dlawjdgid75@naver.com>
2023-07-19 19:48:54 +0900

    (2023.7.19) 자료형 변환

---
+++ Project1/(2023.7.18) 반복문.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include<stdio.h>
+
+void main()
+{
+	// while 문
+	/*
+// 특정 조건을 만족할 때까지 계속해서 주어진 명령문
+// 을 실행하는 반복문입니다.
+
+	int count = 5;
+
+	while (count)
+	{
+		printf("실행 중....\n");
+		count--;
+	}
+
+	while (1)
+	{
+
+	}
+	*/
+
+	// do-while 문
+	/*
+	// 조건과 상관없이 한 번의 작업을 수행한 다음
+	// 조건에 따라 명령문을 실행하는 반복문
+
+	do
+	{
+		printf("게임 시작\n");
+	} while (10 > 20);
+	*/
+
+}
